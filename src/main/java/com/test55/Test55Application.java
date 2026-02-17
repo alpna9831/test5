b51ec1ff9234120243bfa4ff15eaa7023fc4ef94
alpna <alpna9831@gmail.com>
2026-02-17 19:27:06 +0530

    added test55

---
+++ src/main/java/com/test55/Test55Application.java
@@ -0,0 +1,13 @@
+package com.test55;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test55Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test55Application.class, args);
+	}
+
+}
