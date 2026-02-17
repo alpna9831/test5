231d9444abf8d8a35012ad5c4b382645c1934ebe
alpna9831 <alpna9831@gmail.com>
2026-02-17 19:34:10 +0530

    Update Test55Application.java
    added variables

--- src/main/java/com/test55/Test55Application.java
+++ src/main/java/com/test55/Test55Application.java
@@ -6,7 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test55Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+            String name ="mike";
+		    int x=100;
+		    boolean isPresent=false;
+		    int y=200;
+		    char x='a';
+		    double z=10.3;
+		   boolean ispresent=false;
+		   float m =10.3f;
+		 
+		
 		SpringApplication.run(Test55Application.class, args);
 	}
 
